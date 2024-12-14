dd58d92b8184a01ec588e0ab758dede6c717a8d6
i.pakhotin <i.pakhotin@bars-open.ru>
2024-12-14 20:05:43 +0300

    Rewrite script to english

--- src/app/downloadSchema/downloadSchema.js
+++ src/app/downloadSchema/downloadSchema.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 import fs from 'fs/promises';
 
 const URL = 'https://petstore3.swagger.io/api/v3/openapi.yaml';
@@ -7,32 +5,32 @@ const FOLDER = './src/api/schemas/downloads/';
 // const FOLDER = '';
 const FILE_NAME = 'openapi.yaml';
 
-console.log('Шаг 1: Отправка запроса к', URL);
+console.log('Step 1: Sending request to', URL);
 
 try {
     const response = await fetch(URL);
     if (!response.ok) {
-        throw new Error(`Ошибка загрузки: статус ${response.status} (${response.statusText})`);
+        throw new Error(`Error loading: status ${response.status} (${response.statusText})`);
     }
 
-    console.log('Шаг 2: Ответ успешно получен. Чтение данных...');
+    console.log('Step 2: Response received successfully. Reading data...');
     const data = await response.text();
 
-    console.log(`Шаг 3: Сохранение данных в файл "${FOLDER}${FILE_NAME}"...`);
+    console.log(`Step 3: Saving data to file "${FOLDER}${FILE_NAME}"...`);
     await fs.writeFile(FOLDER + FILE_NAME, data, 'utf8');
 
-    console.log('Шаг 4: Данные успешно сохранены.');
+    console.log('Step 4: Data saved successfully.');
 
     const fileStats = await fs.stat(FOLDER + FILE_NAME);
 
-    console.log('--- Метаданные файла ---');
-    console.log('Размер файла:', fileStats.size, 'байт');
-    console.log('Дата последнего изменения:', fileStats.mtime);
-    console.log('Дата создания:', fileStats.birthtime);
-    console.log('------------------------');
+    console.log('--- File Metadata ---');
+    console.log('File size:', fileStats.size, 'bytes');
+    console.log('Last modified date:', fileStats.mtime);
+    console.log('Creation date:', fileStats.birthtime);
+    console.log('---------------------');
 
 } catch (err) {
-    console.error('Произошла ошибка при загрузке или сохранении файла:');
+    console.error('An error occurred while downloading or saving the file:');
     console.error(err.message);
     process.exit(1);
 }
